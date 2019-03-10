ede47e40c7e999efeb6e2f3294f529cdd49d64e0
riaz-123 <44791965+ahmedriaz9908@users.noreply.github.com>
2019-03-10 17:59:29 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
-In ur bot.js file, we'll start off simple. (all code can be found in description)
-all the // are just comments
+//In ur bot.js file, we'll start off simple. (all code can be found in description)
+//all the // are just comments
 ---------------------------------------------------------------------------------
 var discord = require('discord.js') //this is our discord.js library
 var client = new discord.client()
 var prefix = '?' //this is ur bot's prefix. change it to whatever you want
 client.on('message', async message => { //this is an event. it triggers
-                                                                                 //whenever the bot reads a message
+                                      //whenever the bot reads a message
 var command = message.content.slice(prefix.length).trim().split(/ +/g).shift.toLowerCase();
 
 if (command === "hi")
