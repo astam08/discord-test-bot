efd69cae4c0f7a6b7b58ba71f3e56d3c42b8b54f
riaz-123 <44791965+ahmedriaz9908@users.noreply.github.com>
2019-03-11 12:34:28 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,17 @@
-var discord = require('discord.js') //our discord.js library
-var client = new discord.client()  //our bot client
-var prefix = '?' //ur bot's prefix
+var Discord = require('discord.js'); //our discord.js library
+var client = new Discord.Client(); //our bot client
+var prefix = '?'; //ur bot's prefix
 
 client.on('message', async message => { //this is a message event. it triggers when someone sends a message in a dm or server channel.
 
-var command = message.content.slice(prefix.length).trim().split(/ +/g).shift.toLowerCase();
+var args = message.content.slice(prefix.length).trim().split(/ +/g) 
+
+//this is args, the arguments in commands. For example, 
+//?skiddle doodle you're now a noodle 
+// would return ["doodle", "you're", "now", "a", "noodle"]
+
+var command = args.shift().toLowerCase();
+
 //this is our command. it can be any word with our prefix, for example: `?ping` 
 //the command in this case is `ping`
 
@@ -12,11 +19,8 @@ if (command === "ping") { //if you type ?ping in a channel or in the bots dm
 
 message.channel.send('pong!') //sends pong! back
 
-
 }
 
+});
 
-
-
-})
- client.login('UR-BOTS-TOKEN-HERE') //this is the token you get from discords dev portal. it logs in ur bot for you.
+ client.login('NTU0NDM3NTQ2OTU2OTQ3NDU4.D2gwxQ.87tCGGYC7MZSTy7UNOkexedTnfQ'); //this is the token you get from discords dev portal. it logs in ur bot for you.
