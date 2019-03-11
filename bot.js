1e993d8306aef390c878514a01382ea271932416
riaz-123 <44791965+ahmedriaz9908@users.noreply.github.com>
2019-03-11 12:03:31 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,22 @@
-//In ur bot.js file, we'll start off simple. (all code can be found in description)
-//all the // are just comments
-//---------------------------------------------------------------------------------
-var discord = require('discord.js') //this is our discord.js library
-var client = new discord.client()
-var prefix = '?' //this is ur bot's prefix. change it to whatever you want
-client.on('message', async message => { //this is an event. it triggers
-                                      //whenever the bot reads a message
+var discord = require('discord.js') //our discord.js library
+var client = new discord.client()  //our bot client
+var prefix = '?' //ur bot's prefix
+
+client.on('message', async message => { //this is a message event. it triggers when someone sends a message in a dm or server channel.
+
 var command = message.content.slice(prefix.length).trim().split(/ +/g).shift.toLowerCase();
+//this is our command. it can be any word with our prefix, for example: `?ping` 
+//the command in this case is `ping`
 
-if (command === "hi")
-message.channel.send('hi there') //sends hi in the channel the command was sent in
+if (command === "ping") { //if you type ?ping in a channel or in the bots dm
 
-});
+message.channel.send('pong!') //sends pong! back
 
-client.login('YOUR-BOTS-TOKEN-HERE')
+
+}
+
+
+
+
+})
+ client.login('UR-BOTS-TOKEN-HERE') //this is the token you get from discords dev portal. it logs in ur bot for you.
